73985634ee4e2135faeb0388055d3a5700cf69c4
niazieo <omar.niazie@gmail.com>
2023-01-08 16:42:54 -0700

    Updated github link

--- py_version.py
+++ py_version.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.get("https://raw.githubusercontent.com/niazieo/Lab-1/master/py_version.py").text)
+print(requests.get("https://raw.githubusercontent.com/niazieo/CMPUT404-Lab1/master/py_version.py").text)
